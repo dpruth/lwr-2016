799b3e277ca34d711701b2f6c625351503902e8e
dpruth <dpruth001@gmail.com>
2017-12-19 15:39:06 -0600

    Added Secondary Menu registration

--- inc/setup.php
+++ inc/setup.php
@@ -43,6 +43,7 @@ if ( ! function_exists( 'lwr_setup' ) ) :
 		// This theme uses wp_nav_menu() in one location.
 		register_nav_menus( array(
 			'primary' => __( 'Primary Menu', 'lwr' ),
+			'secondary' => __( 'Secondary Menu', 'lwr' ), 
 		) );
 
 		/*
